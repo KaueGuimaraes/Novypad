734453efe09beea5da86209f2565c11d8079c1f9
ACultist <70233917+ACultist@users.noreply.github.com>
2021-08-16 15:02:31 -0300

    test2
    test2

--- Package/main.py
+++ Package/main.py
@@ -1,14 +1,19 @@
 import PySimpleGUI as sg
 from files import *
-print('Teste é o caralho seu A Cultist seu merdinha')
+print('Test')
 
 #Definindo o tema
 theme = 'theme.txt'
 themes = ['Dark', 'DarkAmber', 'DarkBrown1', 'DarkBrown4', 'DarkGrey', 'System Default']
+font = 'font.txt'
 fonts = ['Helvetica', 'Arial']
 if not exist(theme):
     create(theme)
     write(theme, 'DarkBrown')
+
+if not exist(font):
+    create(font)
+    write(font, 'Helvetica')
 
 temaAtual = read(theme)
 sg.ChangeLookAndFeel(temaAtual)
@@ -21,8 +26,11 @@ for c in range(0, len(themes)):
 
 
 about = '''Version 1.3.2
-Authors: A Cultist and Kauê Guimarães'''
-update = '''-Docstring etc'''
+Authors: A Cultist and Kauê Guimarães
+--------------------------//--------------------------------
+A Cultist Instagram: @ldmasterken
+Kauê Guimarães Instagram: @kaueguimaraesk'''
+update = '''- Added Fonts and more...'''
 WIN_W = 90
 WIN_H = 25
 filename = None
@@ -32,13 +40,12 @@ file_open = 'Open..............(CTRL+O)'
 file_save = 'Save............(CTRL+S)'
 
 menu_layout = [['File', [file_new, file_open, file_save, 'Save as', '---', 'Leave']],
-                     ['Settings', ['Themes']],
+                     ['Settings', ['Themes', 'Fonts']],
                      ['Help', ['About', 'New']]]
 
 layout = [[sg.Menu(menu_layout)],
                 [sg.Text('> New NovyTxt <', font=('Consolas', 10), size=(WIN_W, 1), key='_INFO_')],
                 [sg.Multiline(font=('Consolas', 12), size=(WIN_W, WIN_H), key='_BODY_')]]
-
 window = sg.Window('NovyPad 1.3.2', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
 window.read(1)
 window['_BODY_'].expand(True, True)
@@ -65,7 +72,5 @@ while True:
     #Themes
     if event in ('Themes',):
         createThemesWindowButton(themes, 'Themes')
-    #Fonts
-    if event in ('Themes',):
-        createThemesWindowButton(themes, 'Themes')
+
         
